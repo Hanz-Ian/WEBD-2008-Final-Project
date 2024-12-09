17f6ff88ae713ae638f9dc600258d6702c8e7717
Hanz Samonte <hsamonte@academic.rrc.ca>
2024-12-08 23:52:22 -0600

    Added search_form.php
    - For reusing the form for every webpage.

---
+++ search_form.php
@@ -0,0 +1,10 @@
+<!-- /****************      
+    Name: Hanz Samonte
+    Date:  December 7, 2024
+    Description: Final Project - Search Form
+****************/ -->
+
+<form action="search.php" method="get">
+    <input type="text" name="keyword" placeholder="Search..." required>
+    <input type="submit" value="Search">
+</form>
