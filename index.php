bbc425e5804f1c520c1aa52b94d44bbde284a1e9
Hanz Samonte <hsamonte@academic.rrc.ca>
2024-12-10 15:54:51 -0600

    Created CSS file called styles.css.

--- index.php
+++ index.php
@@ -36,6 +36,7 @@ $new_sort_direction = $sort_direction === 'ASC' ? 'desc' : 'asc';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Vintage Archives</title>
 </head>
 <body>
