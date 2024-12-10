c020157e5505b1ff2d6d74084adcd079cb93efb0
Hanz Samonte <hsamonte@academic.rrc.ca>
2024-12-09 23:09:31 -0600

    Updated header.php to implement navigation bars and logic/state of the bar based on the user's state.

--- header.php
+++ header.php
@@ -6,16 +6,32 @@
 
 <header>
     <h1>Vintage Archives</h1>
-    <?php
-    // Check if the current page is not register.php
-    $current_page = basename($_SERVER['PHP_SELF']);
-    if ($current_page !== 'register.php'):
-    ?>
-        <!-- Include the logout link -->
-        <?php include 'logout_link.php'; ?>
-        <!-- Include the login link -->
-        <?php include 'login_link.php'; ?>
-    <?php endif; ?>
     <!-- Include the search form -->
     <?php include 'search_form.php'; ?>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="categories.php">Category</a></li>
+            <!-- If user is admin, show admin priviliges -->
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <?php if ($_SESSION['role'] === 'admin'): ?>
+                    <li class="dropdown">
+                        <a href="#" class="dropbtn"><?= htmlspecialchars($_SESSION['username']) ?></a>
+                        <div class="dropdown-content">
+                            <a href="create_product.php">Create Product</a>
+                            <a href="create_category.php">Create Category</a>
+                        </div>
+                    </li>
+                <!-- Else if user is just a regular logged in user -->
+                <?php else: ?>
+                    <li><a href="#"><?= htmlspecialchars($_SESSION['username']) ?></a></li>
+                <?php endif; ?>
+                <li><a href="logout.php">Logout</a></li>
+            <!-- Else, show the Login or Register -->
+            <?php else: ?>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+            <?php endif; ?>
+        </ul>
+    </nav>
 </header>
