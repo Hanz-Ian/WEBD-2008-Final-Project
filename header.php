7088dcbad827fdfbe2842c7a8380e549ed18d178
Hanz Samonte <hsamonte@academic.rrc.ca>
2024-12-09 19:14:04 -0600

    Added header.php as the website's header.

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>Vintage Archives!</h1>
+    <!-- Include the logout link -->
+    <?php include 'logout_link.php'; ?>
+    <!-- Include the login link -->
+    <?php include 'login_link.php'; ?>
+    <!-- Include the search form -->
+    <?php include 'search_form.php'; ?>
+</header>
